4506cc6219aa100cdbdb44d601c60e5ad8ced142
alavazoel <143626997+alavazoel@users.noreply.github.com>
2024-02-21 15:46:07 +0000

    donuts string

--- concatenation.js
+++ concatenation.js
@@ -9,11 +9,12 @@ submitButton.addEventListener("click", function() {
     let birthYear = document.querySelector(".year").value;
     let age = 2023 - birthYear; // update to 2024, if necessary
     // 1. Declare a variable to save the value of the city the user is from.
-
+let city = document.querySelector(".city").value;
     // 2. Below write your sentences! 
     // - Have them show up in the div with class "results".
+   results.innerHTML= "<li> I am from chicago. I am 15 years old. I like to eat " + food + ".</li>";
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
-
+//<li> I am from chicago. I am 15 years old. I like to eat donut.</li>
 
 });
--- script.js
+++ script.js
@@ -3,13 +3,20 @@ let captionOne = document.querySelector(".caption-one");
 
 buttonOne.addEventListener("click", function() {
     // 1. Declare a variable named spidyCaption.
+  let spidyCaption = document.querySelector(".caption-one");
     //  - Store the value of the input with a class of "comment-one".
-
+class commnetOne = document.querySelector(".comment-one").value;
 
     // 2. Display the variable spidyCaption to captionOne.
-
+let captionOne = document.querySelector(".caption-one").value;
 
 });
 
 // 3. Repeat the previous steps for the second meme.
-//  - Be sure to store variables for the button and caption.
+let hermitCaption = document.querySelector(".caption-one");
+class commnetOne = document.querySelector(".comment-one").value;
+let captionOne = document.querySelector(".caption-one").value;
+
+})
+let buttonTwo = document.querySelector(".button-two");
+let captionTwo = document.querySelector(".caption-two");
